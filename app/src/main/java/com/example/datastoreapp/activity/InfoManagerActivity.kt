b4c17f4ec499150b7638ecfa5dd1c27d9f9e5dbf
SofiaZarzabal <sofia.zarzabal@globant.com>
2022-11-22 11:26:15 -0300

    Main activity and view model implemented and started redirection flow based on stored types

---
+++ app/src/main/java/com/example/datastoreapp/activity/InfoManagerActivity.kt
@@ -0,0 +1,9 @@
+package com.example.datastoreapp.activity
+
+class InfoManagerActivity {
+    // This view will contain the data load/save functionality for the type of method the user wants
+    // These would be:
+        // - Shared preferences
+        // - Proto datastore
+        // - Preferences datastore
+}
