4deb62c4a7f1f2e1c0568bd36a696386b8c81157
SofiaZarzabal <sofia.zarzabal@globant.com>
2022-12-26 21:59:16 -0300

    Add shared preferences functionality

--- app/src/main/java/com/example/datastoreapp/activity/InfoManagerActivity.kt
+++ app/src/main/java/com/example/datastoreapp/activity/InfoManagerActivity.kt
@@ -1,9 +1,38 @@
 package com.example.datastoreapp.activity
 
-class InfoManagerActivity {
-    // This view will contain the data load/save functionality for the type of method the user wants
-    // These would be:
-        // - Shared preferences
-        // - Proto datastore
-        // - Preferences datastore
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.datastoreapp.R
+import com.example.datastoreapp.adapter.ViewPagerAdapter
+import com.example.datastoreapp.databinding.ActivityInfoManagerBinding
+import com.example.datastoreapp.fragment.PreferencesDataStoreFragment
+import com.example.datastoreapp.fragment.ProtoDataStoreFragment
+import com.example.datastoreapp.fragment.SharedPreferencesFragment
+import com.example.datastoreapp.utils.setupWithViewPager2
+
+class InfoManagerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInfoManagerBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInfoManagerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        configTabs()
+    }
+
+    private fun configTabs() = with(binding) {
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPagerAdapter.addFragment(SharedPreferencesFragment(), getString(R.string.txt_shared_preferences_title))
+        viewPagerAdapter.addFragment(PreferencesDataStoreFragment(), getString(R.string.txt_preferences_data_store_title))
+        viewPagerAdapter.addFragment(ProtoDataStoreFragment(), getString(R.string.txt_proto_data_store_title))
+        viewPagerStorageTypes.adapter = viewPagerAdapter
+        tabLayoutStorageTypes.setupWithViewPager2(viewPagerStorageTypes)
+    }
+
+    companion object {
+        fun getIntent(context: Context) = Intent(context, InfoManagerActivity::class.java)
+    }
 }
---
+++ app/src/main/java/com/example/datastoreapp/storetype/SharedPreferencesStore.kt
@@ -0,0 +1,21 @@
+package com.example.datastoreapp.storetype
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferencesStore(private val context: Context) {
+    private val storage: SharedPreferences = context.getSharedPreferences(SP_NAME, 0)
+
+    fun saveName(name: String) {
+        storage.edit().putString(SP_USERNAME, name).apply()
+    }
+
+    fun getName(): String = storage.getString(SP_USERNAME, USERNAME) ?: EMPTY_STRING
+
+    companion object {
+        const val SP_NAME = "SPStore"
+        const val SP_USERNAME = "SP_USERNAME"
+        const val USERNAME = "USERNAME"
+        const val EMPTY_STRING = ""
+    }
+}
