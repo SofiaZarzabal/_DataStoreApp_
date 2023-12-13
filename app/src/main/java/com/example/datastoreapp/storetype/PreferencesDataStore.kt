6516920a1285123150d0242dec383be27c7dbeeb
Sofia Zarzabal <sofia.zarzabal@globant.com>
2023-12-13 10:07:02 -0300

    Add proto and preferences datastore functionality

---
+++ app/src/main/java/com/example/datastoreapp/storetype/PreferencesDataStore.kt
@@ -0,0 +1,28 @@
+package com.example.datastoreapp.storetype
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class PreferencesDataStore(private val context: Context) {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PDS_NAME)
+    private val dataStore = context.dataStore
+    private val pdsAge = intPreferencesKey(PDS_AGE_KEY)
+
+    fun getAge(): Flow<Int> = context.dataStore.data.map { preferences -> preferences[pdsAge] ?: -1 }
+
+    suspend fun setAge(age: Int) {
+        dataStore.edit { preferences -> preferences[pdsAge] = age }
+    }
+
+    companion object {
+        private const val PDS_NAME = "PDSStore"
+        private const val PDS_AGE_KEY = "PDS_AGE_KEY"
+    }
+}
